965200331eda8f2d88dbe3ee987b2d0fd18c7b66
mohamed saba <mohamedhussamsaba@gmail.com>
2023-09-14 12:39:05 -0700

    J

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	float floattype;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(chartype));
-	printf("Size of a int: %zu byte(s)\n", sizeof(inttype));
+	printf("Size of an int: %zu byte(s)\n", sizeof(inttype));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longinttype));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlonginttype));
 	printf("Size of a float: %zu byte(s)\n", sizeof(floattype));
