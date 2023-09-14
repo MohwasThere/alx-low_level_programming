56f3b1d92f700a73238c4717dd31b04e595c64cd
mohamed saba <mohamedhussamsaba@gmail.com>
2023-09-14 12:09:54 -0700

    test

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - Printing using puts()
+ * @void: parameter is void
+ * Return: Always 0 
+ */
+int main(void)
+{
+ 	char text[] = "Programming is like building a multilingual puzzle\n";
+	puts(text);
+     	return (0);
+}
