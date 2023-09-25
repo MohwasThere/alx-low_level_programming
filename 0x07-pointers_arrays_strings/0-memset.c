d9bcb267757c80cecdb6c3119445a3da841b68f8
mohamed saba <mohamedhussamsaba@gmail.com>
2023-09-25 07:49:55 -0700

    memoryoverfil

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_memset - fills memory with a constant byte.
+ * @b: char to copy
+ * @s: memory area to be filled.
+ * @n: number of times to copy b.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for(i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
