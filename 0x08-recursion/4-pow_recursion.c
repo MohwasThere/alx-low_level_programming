4a8b89b0f3807b8e79ae95216e460c31ce0543da
mohamed saba <mohamedhussamsaba@gmail.com>
2023-10-10 13:09:38 -0700

    k

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *  _pow_recursion - returns the value of x 
+ * _pow_recursion - returns the value of x 
  *  raised to the value of y
  *  @x: lower value
  *  @y: upper value
