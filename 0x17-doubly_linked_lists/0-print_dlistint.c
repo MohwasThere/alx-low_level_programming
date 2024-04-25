f4527003c6d7ae484fd35068ab165159c26a1581
root <root@hsabaWin11>
2024-04-25 13:23:00 +0200

    we shoudl walk the linked list back

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all the elements of a d_linkedlist
+ * @h: list to print the elements of
+ * Return: number of nodes
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count;
+	
+	count  = 0;
+
+	if (h == NULL)
+		return (count);
+	
+	while (h->prev != NULL)
+		h = h->prev;
+	
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+using doubly linked lists this time just a linked list with a prev
