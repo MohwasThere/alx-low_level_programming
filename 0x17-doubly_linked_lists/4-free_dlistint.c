dff66c689d70f540c64d355447782170d0d92d4b
root <root@hsabaWin11>
2024-04-25 14:18:27 +0200

    smash

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees the linked list
+ * @head: pointer to linked list head
+ * 
+ * Return : no return
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	if (head != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
+
+	while(head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
