3c6632a0fe8f8f22ac1d154ce43fccd685a98269
root <root@hsabaWin11>
2024-04-25 15:53:12 +0200

    i dont think this will get 100%

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+/**
+ * *insert_dnodeint_at_index - inserst new node at given position
+ * @h: pointer to linked list
+ * @idx: index at where we will insert
+ * @n: value to be inserted
+ * Return: New list, NULL if Fail
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *temp, *new;
+	unsigned int count;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	count = 0;
+
+	new->n = n;
+
+	if (h == NULL)
+		return (NULL);
+
+	temp = *h;
+
+	while (temp->prev != NULL)
+		temp = temp->prev;
+
+	while (count != idx)
+	{
+		temp = temp->next;
+		count++;
+	}
+	if (temp != NULL)
+	{	new->next = temp->next;
+		new->prev = temp;
+		temp->next = new;
+	}
+
+	*h = temp;
+
+	return (new);
+
+}
