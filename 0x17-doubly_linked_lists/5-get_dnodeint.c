4296241ea67d68d800dfc2b6dacfe30388e55878
root <root@hsabaWin11>
2024-04-25 15:37:04 +0200

    lmao 2

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,6 +12,9 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	count = 0;
 
+	if (head == NULL)
+		return (NULL);
+
 	while (head->prev != NULL)
 		head = head->prev;
 
