e533d3407fa8a11f800b07be4d152ba438b33733
root <root@hsabaWin11>
2024-04-25 15:35:53 +0200

    lmao

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * *get_dnodeint_at_index - gets the item and nth node
+ * @head: pointer to the linked list
+ * @index: index of item
+ * Return: node at index, or NULL if not exist
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp;
+	unsigned int count;
+
+	count = 0;
+
+	while (head->prev != NULL)
+		head = head->prev;
+
+	temp = head;
+
+	while (count != index)
+	{
+		if (temp != NULL)
+		{
+			temp = temp->next;
+			count++;
+		}
+		else
+			return (0);
+	}
+
+		return (temp);
+
+}
